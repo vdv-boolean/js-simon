a5734b0ef012bd1ec7320f5c0a3caadaacb1c15f
vincenzodivita <divitavincenzo@icloud.com>
2023-04-27 19:48:21 +0200

    Set script in a functions

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,32 @@
-// Get current date and time
-const currentTime = new Date().getTime();
-
-// Add date of Countdown
+// Set date of Countdown
 const finishDate = new Date("Apr 28, 2023 9:30:00").getTime();
 
-// Calc distance bewteen two dates in ms
-const distanceBetweenDates = finishDate - currentTime;
 
-// Equivalence calc
-const second = 1000;
-const minute = second * 60;
-const hour = minute * 60;
-const day = hour * 24;
+// Function for get distance bewtween date and current time
+function getDistance() {
 
-// Get distance in dd/hh/mm/ss format
-const days = Math.floor(distanceBetweenDates / day);
-const hours = Math.floor(distanceBetweenDates % day / hour);
-const minutes = Math.floor(distanceBetweenDates % hour / minute);
-const seconds = Math.floor(distanceBetweenDates % minute / second);
+    // Get current date and time
+    const currentTime = new Date().getTime();
 
-document.querySelector("h1").innerHTML = days + ":" + hours + ":"
-+ minutes + ":" + seconds;
+    // Calc distance bewteen two dates in ms
+    const distanceBetweenDates = finishDate - currentTime;
+
+    // Equivalence calc
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    // Get distance in dd/hh/mm/ss format
+    const days = Math.floor(distanceBetweenDates / day);
+    const hours = Math.floor(distanceBetweenDates % day / hour);
+    const minutes = Math.floor(distanceBetweenDates % hour / minute);
+    const seconds = Math.floor(distanceBetweenDates % minute / second);
+
+    // Print to DOM
+    document.querySelector("h1").innerHTML = days + ":" + hours + ":"
+    + minutes + ":" + seconds;
+};
+
+// Set refresh every second
+setInterval(getDistance, 1000)
